d9b7c0ff1c2d7decc2681272f7c2948f98797d1c
URK23CS1221 <153969753+URK23CS1221@users.noreply.github.com>
2025-11-04 18:26:31 +0530

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue/svelte, etc.
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/ex7/', // 👈 replace with your repo name
-})
