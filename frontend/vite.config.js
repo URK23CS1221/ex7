8733442154a92eca3070bd1df97dc73ff385d9dc
URK23CS1221 <153969753+URK23CS1221@users.noreply.github.com>
2025-11-04 18:08:36 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or vue/svelte, etc.
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ex7', // 👈 replace with your repo name
 })
